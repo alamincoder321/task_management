c98a00752be2542401f40ead1208ba44bfa7655a
alamincoder2001 <114378678+alamincoder2001@users.noreply.github.com>
2024-07-29 16:00:56 +0600

    update

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -20,7 +20,7 @@ class RedirectIfAuthenticated
     public function handle(Request $request, Closure $next, $guard = null)
     {
         if (Auth::guard("web")->check()) {
-            return redirect('/module/dashboard');
+            return redirect('/dashboard');
         }
 
         return $next($request);
